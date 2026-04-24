53b6cd93048b920b7309782d403c283e54ed732a
Fer <fernandomm1840@gmail.com>
2026-04-24 20:20:42 +0200

    (fix): Arreglada la imagen OG de genesis

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'node:path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        genesis: resolve(__dirname, 'genesis.html'),
+      },
+    },
+  },
 })
